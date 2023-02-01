2a71b51cc13ff9546e3b3b217a4c81a03dde2854
Eric <eric_liao@owlting.com>
2023-02-01 06:35:45 +0000

    💡 feat: add log

--- Services/GreeterService.cs
+++ Services/GreeterService.cs
@@ -13,6 +13,8 @@ public class GreeterService : Greeter.GreeterBase
 
     public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
     {
+        _logger.LogInformation("call SayHello at {DT}.", DateTime.UtcNow.ToLongTimeString());
+
         return Task.FromResult(new HelloReply
         {
             Message = "Hello " + request.Name
